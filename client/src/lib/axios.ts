4664d614e2dff0f0b9dc35a26c5b72277a9eca94
Eduardo Oliveira <eduardo_y05@outlook.com>
2025-07-07 20:54:34 -0300

    refactor: remove csrf token from front-end

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,26 +1,9 @@
 import axios from 'axios';
-import { generateCSRFToken, getCSRFSecret } from './csrf-token';
 
 const api = axios.create({
   baseURL: 'http://localhost:8000/api',
   adapter: 'fetch',
   withCredentials: true
 })
-
-api.interceptors.request.use((req) => {
-  if (!req.method) {
-    return req;
-  }
-  if (!['PUT', 'POST', 'PATCH'].includes(req.method.toUpperCase())) {
-    return req;
-  }
-  const secret = getCSRFSecret();
-  if (!secret) {
-    return req;
-  }
-  const token = generateCSRFToken(secret);
-  req.headers['X-CSRFToken'] = token;
-  return req;
-});
 
 export default api;
